76cc57c4df524b714e0d5f98f1c4df9ebfb139f2
Mathias Palfinger <mathias.palfinger@stud.fh-campuswien.ac.at>
2022-03-25 19:45:50 +0100

    Tests added

--- src/main/java/FuelCalculator.java
+++ src/main/java/FuelCalculator.java
@@ -10,7 +10,7 @@ public class FuelCalculator {
 
     protected static int countLine(File PuzzleInput) {
         int lines = 0;
-        Path path = Paths.get("C:\\Users\\mathi\\IdeaProjects\\prog2-ss21-exercise1\\src\\test\\resources\\PuzzleInput");
+        Path path = Paths.get("\\PuzzleInput.txt");
         try {
             lines = (int) Files.lines(path).count();
         } catch (IOException e) {
@@ -22,4 +22,42 @@ public class FuelCalculator {
     protected static int fuelRequirement(int moduleMass) {
         return (moduleMass/3)-2;
     }
+
+    protected static int sumFuel (int sum) throws FileNotFoundException {
+        File file = new File("C:\\Users\\mathi\\IdeaProjects\\prog2-ss21-exercise1\\src\\test\\resources\\PuzzleInput.txt");
+        Scanner sc = new Scanner(file);
+            while(sc.hasNext()) {
+                int fuel = fuelRequirement(sc.nextInt());
+                sum += fuel;
+            }
+        return sum;
+    }
+
+    protected static int totalFuelPerModule (int mass) {
+        int total = 0;
+        int fuel = (mass/3)-2;
+        int fuelTotal = fuel;
+        while(fuel>0) {
+            fuel = (fuel/3)-2;
+            if(fuel>0) {
+                fuelTotal += fuel;
+            }
+        }
+        total += fuelTotal;
+
+        return total;
+    }
+    protected static int sumTotalFuel (int total) throws FileNotFoundException {
+
+        File file = new File("C:\\Users\\mathi\\IdeaProjects\\prog2-ss21-exercise1\\src\\test\\resources\\PuzzleInput.txt");
+        Scanner sc = new Scanner(file);
+        while(sc.hasNext()) {
+            total += totalFuelPerModule(sc.nextInt());
+        }
+        return total;
+    }
+
+
+
+
 }
--- src/test/java/FuelCalculatorTest.java
+++ src/test/java/FuelCalculatorTest.java
@@ -23,19 +23,14 @@ public class FuelCalculatorTest {
 
     @Test
     void file_is_available(){
-        File f = new File("C:\\Users\\mathi\\IdeaProjects\\prog2-ss21-exercise1\\src\\test\\resources\\PuzzleInput");
+        File f = new File("C:\\Users\\mathi\\IdeaProjects\\prog2-ss21-exercise1\\src\\test\\resources\\PuzzleInput.txt");
         assertTrue(f.exists());
     }
 
     @Test
     void file_is_not_available(){
-        File f = new File("C:\\Users\\mathi\\IdeaProjects\\prog2-ss21-exercise1\\src\\test\\resources\\PuzzleInput2");
+        File f = new File("C:\\Users\\mathi\\IdeaProjects\\prog2-ss21-exercise1\\src\\test\\resources\\PuzzleInput2.txt");
         assertFalse(f.exists());
-    }
-
-    @Test
-    void file_has_modules() {
-
     }
 
     @Test
@@ -49,21 +44,49 @@ public class FuelCalculatorTest {
     }
 
     @Test
-    void modules_are_not_0(){
-
-    }
-
-    @Test
-    void modules_are_not_negative(){
-
-    }
-
-    @Test
-    void fuelRequirement_works_correct() {
+    void fuelRequirement_Scenario_12() {
         assertEquals(FuelCalculator.fuelRequirement(12),2);
+    }
+
+    @Test
+    void fuelRequirement_Scenario_14() {
         assertEquals(FuelCalculator.fuelRequirement(14),2);
+    }
+
+    @Test
+    void fuelRequirement_Scenario_1969() {
         assertEquals(FuelCalculator.fuelRequirement(1969),654);
+    }
+
+    @Test
+    void fuelRequirement_Scenario_100756() {
         assertEquals(FuelCalculator.fuelRequirement(100756),33583);
     }
+
+    @Test
+    void sumFuel() throws FileNotFoundException {
+        assertEquals(FuelCalculator.sumFuel(0),3297866);
+    }
+
+    @Test
+    void fuelPerModule_Scenario_14() {
+        assertEquals(FuelCalculator.totalFuelPerModule(14),2);
+    }
+
+    @Test
+    void fuelPerModule_Scenario_1969() {
+        assertEquals(FuelCalculator.totalFuelPerModule(1969),966);
+    }
+
+    @Test
+    void fuelPerModule_Scenario_100756() {
+        assertEquals(FuelCalculator.totalFuelPerModule(100756),50346);
+    }
+
+    @Test
+    void sumTotalFuel() throws FileNotFoundException {
+        assertEquals(FuelCalculator.sumTotalFuel(0),4943923);
+    }
+
 
 }
